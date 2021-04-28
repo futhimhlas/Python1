cc67118731933b1be9e6580479b839efab4212de
futhimhlas <futhimhl2@gmail.com>
2021-04-28 11:33:31 +0000

    Flask app succesfully running

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
-from flask import Flask
+import os
+from flask import Flask # importing class from flask, capital is important
 
-app = Flask(__name__)
+app = Flask(__name__) # creating an instance of this as a variable called app. the first arguement is the name of the applications module/package
 
-@app.route("/")
+@app.route("/") # decorator (@) a way of wrapping functions
 def index():
-    return "Hello, World!"
+    return "Hello, World!" # / indicates root directory which wuld then cause flask to trigger the index function designated under our app decorator
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
+    )
