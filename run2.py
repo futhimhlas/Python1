ffeda5bdbc729112cded27a9a965be14b1e7d2cd
futhimhlas <futhimhl2@gmail.com>
2021-05-03 14:50:19 +0000

    Final commit. Project in deployment state

--- run2.py
+++ run2.py
@@ -1,8 +1,13 @@
 import os
 import json
-from flask import Flask, render_template # the render template function allows us to not write all of our html in a python file
+from flask import Flask, render_template, request, flash # the render template function allows us to not write all of our html in a python file
+# request finds out what method we used and contains our form object when we've posted it. flash is used to display a message that will leave after navigating to a new page or reloading
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/") 
 def index():
@@ -19,11 +24,11 @@ def about():
     return render_template("about.html", page_title="About", company = data) # render_template takes as many arguemnts as you like. Variable name page_title could have been anything
 
 
-@app.route("/about/<member_name>") # Angle brakcets will pas in the data from ou url path into the view below
-def about_member(member_name):
+@app.route("/about/<member_name>") # Angle brakcets will pass in the data from the url path into the view below
+def about_member(member_name): # is the same member_name from above. So whenever we route from about into something deeper, we are using this view
     member={}
-    with open("data/company.json", "r") as json_data:
-        data = json.load(json_data)
+    with open("data/company.json", "r") as json_data: # opening our json file in read only access
+        data = json.load(json_data) # here we pass the data through and convert it into JSON and store it in a python array
         for object in data:
             if object["url"] == member_name:
                 member = object 
@@ -34,8 +39,10 @@ def about_member(member_name):
 def careers():
     return render_template("careers.html", page_title="Careers")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message".format(request.form.get("name"))) # Output is a dictionary therefore dictionary methods are used to accesss data
     return render_template("contact.html", page_title="Contact")
 
 # Two blank lines between each python function is good code practice
